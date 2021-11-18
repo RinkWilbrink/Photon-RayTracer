32938ab03a5ac68bfc6d3fdb4b7389be538922b8
Rink Wilbrink <rinkwilbrink1@gmail.com>
2021-11-18 15:12:26 +0100

    Listing 11

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -1,3 +1,4 @@
 @echo off
+del "C:\Users\rinkw\Documents\School And Work\repos\Photon_RayTracer\Builds\Release\*.ppm"
 cd C:\Users\rinkw\Documents\School And Work\repos\Photon_RayTracer\Builds\Release
 Photon_RayTracer.exe > image.ppm
--- Photon_RayTracer/src/main.cpp
+++ Photon_RayTracer/src/main.cpp
@@ -3,10 +3,31 @@
 #include "Colour/Colour.h"
 #include "Ray/Ray.h"
 
+double hit_sphere(const Point3& center, double radius, const Ray& r)
+{
+    Vector3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b * b - 4 * a * c;
+
+    if(discriminant >= 0)
+    {
+        return (- b - sqrt(discriminant)) / (2.0 * a);
+    }
+    return -1.0;
+}
+
 Colour ray_colour(const Ray& r)
 {
+    auto t = hit_sphere(Point3(0, 0, -1), 0.5, r);
+    if(t > 0.0)
+    {
+        Vector3 N = unit_vector(r.at(t) - Vector3(0, 0, -1));
+        return 0.5 * Colour(N.X() + 1, N.Y() + 1, N.Z() + 1);
+    }
     Vector3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y + 1.0);
+    t = 0.5 * (unit_direction.Y() + 1.0);
     return (1.0 - t) * Colour(1.0, 1.0, 1.0) + t * Colour(0.5, 0.7, 1.0);
 }
 
