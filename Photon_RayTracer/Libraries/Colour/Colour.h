43229f876ea5dfb163b617e9f4f4123aea88dce4
Rink Wilbrink <rinkwilbrink1@gmail.com>
2021-11-19 13:33:34 +0100

    Chapter 7: AntiAlliasing

--- Photon_RayTracer/Libraries/Colour/Colour.h
+++ Photon_RayTracer/Libraries/Colour/Colour.h
@@ -3,9 +3,19 @@
 
 #include <iostream>
 
-void write_colour(std::ostream& out, Colour pixel_colour)
+void write_colour(std::ostream& out, Colour pixel_colour, int samples_per_pixel)
 {
-	out << static_cast<int>(255.999 * pixel_colour.x) << ' '
-		<< static_cast<int>(255.999 * pixel_colour.y) << ' '
-		<< static_cast<int>(255.999 * pixel_colour.z) << '\n';
+	auto r = pixel_colour.x;
+	auto g = pixel_colour.y;
+	auto b = pixel_colour.z;
+
+	//
+	auto scale = 1.0 / samples_per_pixel;
+	r *= scale;
+	g *= scale;
+	b *= scale;
+
+	out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+		<< static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+		<< static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
--- Photon_RayTracer/src/main.cpp
+++ Photon_RayTracer/src/main.cpp
@@ -1,6 +1,8 @@
 #include <precomp.h>
 #include "Libraries/rtweekend/rtweekend.h"
 
+#include "Camera/Camera.h"
+
 #include "Libraries/Colour/Colour.h"
 #include "Hittable/hittable_list.h"
 #include "Sphere.h"
@@ -23,6 +25,7 @@ int main(int argc, char* argv[])
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -30,14 +33,7 @@ int main(int argc, char* argv[])
     world.Add(make_shared<Sphere>(Point3(0, -100.5, -1), 100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = Point3(0, 0, 0);
-    auto horizontal = Vector3(viewport_width, 0, 0);
-    auto vertical = Vector3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vector3(0, 0, focal_length);
+    Camera cam;
 
     // Render
 
@@ -49,12 +45,16 @@ int main(int argc, char* argv[])
 
         for (int x = 0; x < image_width; ++x) // i
         {
-            auto u = double(x) / (image_width - 1);
-            auto v = double(y) / (image_height - 1);
-            Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            Colour pixel_colour(0, 0, 0);
+            for(int s = 0; s < samples_per_pixel; s++)
+            {
+                auto u = (x + random_double()) / (image_width - 1);
+                auto v = (y + random_double()) / (image_height - 1);
+                Ray r = cam.get_ray(u, v);
+                pixel_colour += ray_colour(r, world);
+            }
 
-            Colour pixel_colour = ray_colour(r, world);
-            write_colour(std::cout, pixel_colour);
+            write_colour(std::cout, pixel_colour, samples_per_pixel);
         }
     }
 
